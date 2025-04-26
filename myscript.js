4315c77bb539cb817fc995a744f3e2f0c44e2c5a
shivansh sharma <105637680+Shivansh-Sharma-134@users.noreply.github.com>
2025-04-26 20:18:41 +0530

    main logic ready

--- myscript.js
+++ myscript.js
@@ -1,53 +1,54 @@
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
-const scissorBtn = document.getElementById("scissor");
+const scissorBtn = document.getElementById("scissors");
+const results = document.getElementById("results");
 const choice=["rock","paper","scissors"]
 let humanScore =0;
 let computerScore=0;
+let i =0;
 const getComputerChoice = ()=>{
     return choice[Math.floor(Math.random()*3)]
 }
-const getHumanChoice=()=>{
-    return prompt("please enter your choice\n1. Rock\n2. Paper\n3. Scissors");
-}
 
 const playRound =(humanChoice, computerChoice)=>{
-    if(humanChoice.toLowerCase() == computerChoice){
+    if(i==5){
+      humanScore>computerScore? results.innerHTML=`<h4>You Win the Game!!!</h4>`:humanScore<computerScore? console.log("You Lose The Game"):console.log("Its a Draw!!");
+      i=0;
+      return;
+    }
+    else if(humanChoice.toLowerCase() == computerChoice){
         console.log("Draw" )
+        i++;
         return;
     }
     else if(humanChoice.toLowerCase() == "rock" && computerChoice =="scissors"){ 
         humanScore++;
         console.log("you win computer chose:"+computerChoice)
+        i++;
         return;
     }
     else if(humanChoice.toLowerCase() == "paper" && computerChoice =="rock"){
         humanScore++;
         console.log("you win computer chose:"+computerChoice)
+        i++;
         return;
     }
     else if(humanChoice.toLowerCase() == "scissors" &&computerChoice =="paper"){
         humanScore++
         console.log("you win computer chose:"+computerChoice)
+        i++;
         return;
     }
     else {
         computerScore++
         console.log("you lose computer chose:"+computerChoice)
+        i++;
         return;
     }
 }
-function playGame(){
-    let i = 0;
-    while(i<5){
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-console.log(Math.floor(Math.random()*3))
-playRound(humanSelection, computerSelection);    
-i++
-}
-humanScore>computerScore? console.log("You Win the Game!!!"):humanScore<computerScore? console.log("You Lose The Game"):console.log("Its a Draw!!");
-}
-rockBtn.addEventListener("click",playRound);
-paperBtn.addEventListener("click",playRound);
-scissorBtn.addEventListener("click",playRound);
+
+    
+
+rockBtn.addEventListener("click",()=>playRound("rock",getComputerChoice()));
+paperBtn.addEventListener("click",()=>playRound("paper",getComputerChoice()));
+scissorBtn.addEventListener("click",()=>playRound("scissors",getComputerChoice()));
